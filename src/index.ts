2f942235872d4bd465f13acfe3ee95005d8d458c
Uri Shaked <uri@urishaked.com>
2018-03-30 23:27:03 +0300

    fix: improve type coverage calculation
    don't count function and class declearations when calculating the type coverage.
    Also return the type coverage percentage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './apply-types';
+export * from './compiler-helper';
 export * from './instrument';
 export * from './node-register';
 export * from './type-collector';
+export * from './type-coverage';
